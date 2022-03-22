8a4787a0a6f3cd38bb84c36ead05eda11b459ce8
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-03-22 15:34:19 +0530

    Using clean architecture

--- app/src/main/java/com/example/dictionaryapp/feature_dictionary/data/local/WordInfoDatabase.kt
+++ app/src/main/java/com/example/dictionaryapp/feature_dictionary/data/local/WordInfoDatabase.kt
@@ -1,4 +1,13 @@
 package com.example.dictionaryapp.feature_dictionary.data.local
 
-class WordInfoDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.dictionaryapp.feature_dictionary.data.local.entity.WordInfoEntity
+
+@Database(
+    entities = [WordInfoEntity::class],
+    version = 1
+)
+abstract class WordInfoDatabase: RoomDatabase() {
+    abstract  val dao: WordInfoDoa
 }
