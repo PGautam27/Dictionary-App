408dd1a44c66d3703d4e0c44d1ae782ea6fe5039
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-03-22 15:58:05 +0530

    Using clean architecture

--- app/src/main/java/com/example/dictionaryapp/feature_dictionary/data/repository/WordInfoRepositoryImpl.kt
+++ app/src/main/java/com/example/dictionaryapp/feature_dictionary/data/repository/WordInfoRepositoryImpl.kt
@@ -1,4 +1,31 @@
 package com.example.dictionaryapp.feature_dictionary.data.repository
 
-class WordInfoRepositoryImpl {
+import com.example.dictionaryapp.core.util.Resource
+import com.example.dictionaryapp.feature_dictionary.data.local.WordInfoDoa
+import com.example.dictionaryapp.feature_dictionary.data.remote.DictionaryApi
+import com.example.dictionaryapp.feature_dictionary.domain.model.WordInfo
+import com.example.dictionaryapp.feature_dictionary.domain.repository.WordInfoRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+
+class WordInfoRepositoryImpl(
+    private val api: DictionaryApi,
+    private val dao: WordInfoDoa
+): WordInfoRepository {
+    override fun getWordInfo(word: String): Flow<Resource<List<WordInfo>>> = flow {
+        emit(Resource.Loading())
+
+        val worInfos = dao.getWordInfos(word).map { it.toWordInfo() }
+        emit(Resource.Loading(data = worInfos))
+
+        try {
+            val remoteWordInfos = api.getWordInfo()
+            dao.delteWordInfos(remoteWordInfos.map { it.word })
+            dao.insertWordInfos(remoteWordInfos.map { it.toWordInfoEntity() })
+        }catch (e: HttpException){
+
+        }catch (e: IOException)
+    }
 }
